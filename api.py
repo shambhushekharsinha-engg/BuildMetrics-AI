60f124577ee50f7aeea2d616a9cc114df5a89890
Shambhu Shekhar Sinha <shambhushekharsinha@example.com>
2026-09-12 22:41:02 +0530

    fix: compile missing requirements and secure CORS origin config

--- api.py
+++ api.py
@@ -18,7 +18,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
